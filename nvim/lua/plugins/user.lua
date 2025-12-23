3ad8543b1c1509f62bc6249979031cb2b839f175
yupiman <yukilv519@gmail.com>
2025-12-24 02:14:19 +0900

    Update config: 2025-12-24 02:14:19

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
 -- Here are some examples:
@@ -84,5 +82,36 @@ return {
         Rule("a", "a", "-vim")
       )
     end,
+  },
+  -- 透明化プラグインを追加
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    opts = {
+      -- Neo-tree やステータスラインなども透明化の対象に加える
+      extra_groups = {
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "NeoTreeEndOfBuffer",
+        "NeoTreeFloatNormal",
+        "NeoTreeWinSeparator",
+        "NormalFloat", -- フローティングウィンドウも透明にしたい場合
+        "TabLine", -- 上部のタブバー
+        "TabLineFill",
+        "StatusLine",
+        "StatusLineNC",
+        "WinBar",
+        "WinBarNC",
+        -- ステータスバー（AstroVim専用）
+        "StatusLine",
+        "StatusLineNC",
+        "AstroStatusLine",
+        "AstroStatusLineNC",
+        "AstroStatusLineCustom",
+        -- 境界線やその他
+        "WinSeparator",
+        "VertSplit",
+      },
+    },
   },
 }
